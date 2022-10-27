fbce2050adc4041c4c0c7d84be484c7d44e52c24
418181 <115142558+418181@users.noreply.github.com>
2022-10-27 16:14:51 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
+function 燈號2 (num: number) {
+    x = 0
+    y = 0
+    if (num < 6) {
+        x = num - 1
+        y = 0
+    } else if (num >= 6 && num < 9) {
+        x = 4
+        y = num - 5
+    } else if (num >= 9 && num < 14) {
+        x = 13 - num
+        y = 4
+    } else if (num >= 14 && num < 17) {
+        x = 0
+        y = 17 - num
+    } else if (num >= 17 && num < 20) {
+        x = num - 16
+        y = 1
+    } else if (num >= 20 && num < 22) {
+        x = 3
+        y = num - 18
+    } else if (num >= 22 && num < 24) {
+        x = 24 - num
+        y = 3
+    } else if (num >= 24 && num < 25) {
+        x = 1
+        y = num - 22
+    } else if (num == 25) {
+        x = 2
+        y = 2
+    }
+    led.unplot(x, y)
+}
 function 燈號 (num: number) {
     x = 0
     y = 0
@@ -10,23 +43,24 @@ function 燈號 (num: number) {
     } else if (num >= 9 && num < 14) {
         x = 13 - num
         y = 4
-    } else if (num >= 14 && num <= 16) {
+    } else if (num >= 14 && num < 17) {
         x = 0
         y = 17 - num
-    } else if (num >= 17 && num <= 19) {
+    } else if (num >= 17 && num < 20) {
         x = num - 16
         y = 1
-    } else if (num == 20) {
+    } else if (num >= 20 && num < 22) {
         x = 3
-        y = 22 - num
-    } else if (num >= 21 && num <= 23) {
+        y = num - 18
+    } else if (num >= 22 && num < 24) {
         x = 24 - num
         y = 3
-    } else if (num >= 24 && num <= 25) {
-        x = num - 23
+    } else if (num >= 24 && num < 25) {
+        x = 1
+        y = num - 22
+    } else if (num == 25) {
+        x = 2
         y = 2
-    } else {
-    	
     }
     led.plot(x, y)
 }
@@ -34,7 +68,16 @@ let y = 0
 let x = 0
 let a = 0
 basic.forever(function () {
+    a = 0
+    for (let index = 0; index < 25; index++) {
+        a += 1
+        燈號(a)
+        basic.pause(100)
+    }
     a += 1
-    燈號(a)
-    basic.pause(100)
+    for (let index = 0; index < 25; index++) {
+        a += -1
+        燈號2(a)
+        basic.pause(100)
+    }
 })
